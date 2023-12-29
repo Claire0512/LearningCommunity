60104741fa86f68306f0543196803e88d009997c
enip <enip2473@gmail.com>
2023-12-29 10:26:09 +0800

    Support inserting multiple tags

---
+++ src/app/api/tags/multiple/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+import { z } from 'zod';
+
+import { db } from '@/db';
+import { tagsTable } from '@/db/schema';
+
+const PostRequestSchema = z.object({
+	names: z.array(z.string()),
+});
+
+type PostRequestType = z.infer<typeof PostRequestSchema>;
+
+export async function POST(req: NextRequest) {
+	const data = await req.json();
+	try {
+		PostRequestSchema.parse(data);
+	} catch (error) {
+		console.error('Error parsing request in api/sign-up/route.ts');
+		return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+	}
+	const { names } = data as PostRequestType;
+
+
+    const response = names.map(async name => {
+        const tagExists = await db.query.tagsTable.findFirst({
+            where: (existTag, { eq }) => eq(existTag.name, name),
+        });
+        if (tagExists) return tagExists.tagId;    
+        const [newTag] = await db
+            .insert(tagsTable)
+            .values({ name })
+            .returning({ tagId: tagsTable.tagId });
+        return newTag.tagId;
+    })
+
+    const result = await Promise.all(response)
+    return NextResponse.json(result, { status: 201 });
+}
